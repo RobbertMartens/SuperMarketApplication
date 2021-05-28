38317ec4cd07b64f76f5a510b834f7150e947364
Robbert Martens <Robbertmartens@gmail.com>
2021-05-28 16:07:37 +0200

    Added ProductService unit test

--- Service.UnitTests/Services/ProductServiceTests.cs
+++ Service.UnitTests/Services/ProductServiceTests.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
+using Service.Enum;
 using Service.Interfaces;
 using Service.Models;
 using Service.Repositories;
+using Service.Services;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Service.UnitTests.Services
 {
@@ -11,37 +16,40 @@ namespace Service.UnitTests.Services
     {
         private Mock<DbSet<Product>> _mockSet;
         private Mock<ProductContext> _mockContext;
+        private Mock<DbContextOptions> _mockDbContextOptions;
         private IProductService _productService;
 
         [SetUp]
         public void Init()
         {
+            _mockDbContextOptions = new Mock<DbContextOptions>(It.IsAny<IReadOnlyDictionary<It.IsAnyType, It.IsAnyType>>());
             _mockSet = new Mock<DbSet<Product>>();
-            _mockContext = new Mock<ProductContext>();
+            _mockContext = new Mock<ProductContext>(new DbContextOptionsBuilder().Options);
+            
         }
 
-        //[Test]
-        //public void AddProductTest_ShouldReturnOneRowAffected()
-        //{
-        //    // Assemble
-        //    var product = new ProductDB
-        //    {
-        //        ProductName = "Test product",
-        //        Barcode = 12345,
-        //        Price = 1.99M,
-        //        Discount = Discount.NoDiscount,
-        //        Amount = 1
-        //    };
+        [Test]
+        public async Task AddProductTest_ShouldReturnOneRowAffected()
+        {
+            // Assemble
+            var product = new Product
+            {
+                ProductName = "Test product",
+                Barcode = 12345,
+                Price = 1.99M,
+                Discount = Discount.NoDiscount,
+                Amount = 1
+            };
 
-        //    _mockContext.Setup(m => m.Product).Returns(() =>_mockSet.Object);
-        //    _productService = new ProductService(_mockContext.Object);
+            _mockContext.Setup(m => m.Product).Returns(() => _mockSet.Object);
+            _productService = new ProductService(_mockContext.Object);
 
-        //    // Act
-        //    _productService.InsertProduct(product);
+            // Act
+            await _productService.InsertProduct(product);
 
-        //    // Assert
-        //    _mockSet.Verify(m => m.Add(It.IsAny<ProductDB>()), Times.Once);
-        //    _mockContext.Verify(m => m.SaveChanges(), Times.Once);
-        //}
+            // Assert
+            _mockSet.Verify(mock => mock.AddAsync(product, It.IsAny<CancellationToken>()), Times.Once);
+            _mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
     }
 }
